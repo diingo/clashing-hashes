71dd327ee5af83fc790dbab6a9d32fb3c4b3cd5e
Gene Le <gene.gkl@gmail.com>
2013-09-27 13:47:58 -0500

    Initial Commit

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,22 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'twitter'
+
+require_relative 'lib/twitter'
 
 get '/' do
-  'hello world'
+  @name = 'Bo'
+  @show_description = false
+
+  erb :hello
 end
+
+post '/twitter' do
+  @tweets = Twitter::Tweet.give_me_tweets(20)
+  @first = params[:first]
+  @second = params[:second]
+
+  erb :twitter_results
+end
+
